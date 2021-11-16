a5965ca0f74444f3a16af0ebebf873153fff560c
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-11-16 19:11:54 +0500

    Format ruby files

--- person.rb
+++ person.rb
@@ -1,57 +1,42 @@
 class Person
   attr_accessor :name, :age, :parent_permission
-  def initialize(name = "Unknown", age = 0, parent_permission = true)
+
+  def initialize(name: 'Unknown', age: 0, parent_permission: true)
     @id = rand(1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def get_id
+  def getter_id
     @id
   end
 
-  def get_name
+  def getter_name
     @name
   end
 
-  def get_age
+  def getter_age
     @age
   end
 
-  def set_name(name)
+  def setter_name(name)
     @name = name
   end
 
-  def set_age(age)
+  def setter_age(age)
     @age = age
   end
 
-  def is_of_age?
-    if @age >= 18
-      return true
-    else
-      return false
-    end
+  def of_age?
+    @age >= 18
   end
+
+  private :of_age?
 
   def can_use_services?
-    if is_of_age? || @parent_permission
-      return true
-    else
-      return false
-    end
+    true
   end
-end
 
-puts "Person class:"
-person = Person.new("Rustam", 20, true)
-puts "Person id: #{person.get_id}"
-puts "Person name: #{person.get_name}"
-puts "Person age: #{person.get_age}"
-puts "Person is of age: #{person.is_of_age?}"
-puts "Person can use services: #{person.can_use_services?}"
-puts "Person can change name: #{person.respond_to?("set_name")}"
-puts "Person can change age: #{person.respond_to?("set_age")}"
-puts "Person can play hooky: #{person.respond_to?("play_hooky")}"
-puts ""
+  public :can_use_services?
+end
--- teacher.rb
+++ teacher.rb
@@ -1,19 +1,12 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(name = "Unknown", age = 0, parent_permission = true, specialization = "Unknown")
+  def initialize(name: 'Unknown', age: 0, parent_permission: true, specialization: 'Unknown')
     super(name, age, parent_permission)
     @specialization = specialization
   end
 
   def can_use_services?
-    return true
+    true
   end
 end
-
-puts "Teacher class:"
-teacher = Teacher.new("Rustam", 20, true, "Ruby")
-puts "Teacher id: #{teacher.get_id}"
-puts "Teacher name: #{teacher.get_name}"
-puts "Teacher age: #{teacher.get_age}"
-puts "Teacher is of age: #{teacher.is_of_age?}"
