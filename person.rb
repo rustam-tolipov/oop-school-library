7c9d732c7f27b833cacda63eda808f731503f6d3
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-11-17 18:30:34 +0500

    Create connection between `Person` and `Rental`

--- person.rb
+++ person.rb
@@ -1,31 +1,33 @@
-require_relative 'corrector'
+require './corrector'
 
 class Person
-  attr_accessor :name, :age, :parent_permission, :id
+  attr_accessor :name, :age, :parent_permission, :rentals
+  attr_reader :id, :corrector
 
-  def initialize(name: 'Unknown', age: 0, parent_permission: true)
+  def initialize(name, age, parent_permission = { parent_permission: true })
     @id = rand(1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @corrector = Corrector.new(@name)
+    @corrector = Corrector.new
+    @rentals = []
+  end
+
+  def of_age?
+    @age >= 18
   end
 
   def can_use_services?
-    true
+    of_age && parent_permission[:parent_permission]
   end
 
   def validate_name
     @name = @corrector.correct_name
   end
 
-  private
-
-  def of_age?
-    @age >= 18
+  def add_rental(date, book)
+    Rental.new(date, book)
   end
-end
 
-person = Person.new(name: 'Rustamjon', age: 19)
-person.validate_name
-puts person.name
+  private :of_age?
+end
