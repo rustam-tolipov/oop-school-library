faaea656a018595e50b646ef452b90a04a33d156
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-11-27 16:08:15 +0500

    Fix linters errors

--- spec/rentals_spec.rb
+++ spec/rentals_spec.rb
@@ -30,7 +30,8 @@ require 'rspec'
 RSpec.describe Rental do
   describe '#initialize' do
     it 'should initialize a rental with a date' do
-      rental = Rental.new('2018-05-01', Book.new('The Hobbit', 'J.R.R. Tolkien'), Student.new('Rustamjon', 20, { parent_permission: true }))
+      rental = Rental.new('2018-05-01', Book.new('The Hobbit', 'J.R.R. Tolkien'),
+                          Student.new('Rustamjon', 20, { parent_permission: true }))
       expect(rental.date).to eq('2018-05-01')
     end
   end
