ebb49620bec072b778a5ad2b1ea83a4a58fd4299
Ahmad Zia Yousufi <ah.ziayosfi@gmail.com>
2021-11-27 14:10:08 +0430

    add context info about tests

--- spec/corrector_spec.rb
+++ spec/corrector_spec.rb
@@ -2,16 +2,18 @@ require './corrector'
 require 'rspec'
 
 RSpec.describe Corrector do
-  before(:each) do
-    @corrector = Corrector.new
-  end
+  context “When testing the corrector” do
+    before(:each) do
+      @corrector = Corrector.new
+    end
 
-  it 'should capitalize first letter test1:ahmad' do
-    expect(@corrector.correct_name('ahmad')).to eq('Ahmad')
-  end
+    it 'should capitalize first letter test1:ahmad' do
+      expect(@corrector.correct_name('ahmad')).to eq('Ahmad')
+    end
 
-  it 'should capitalize first letter test2:rustamjon' do
-    expect(@corrector.correct_name('rustamjon')).to eq('Rustamjon')
+    it 'should capitalize first letter test2:rustamjon' do
+      expect(@corrector.correct_name('rustamjon')).to eq('Rustamjon')
+    end
   end
 end
 
