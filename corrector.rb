d3dada40fe167818d91a9a91c2adaa2c91ddfe95
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-11-27 12:05:05 +0500

    Make corrector.rb testable

--- corrector.rb
+++ corrector.rb
@@ -1,6 +1,7 @@
 class Corrector
   def correct_name(name)
-    name.capitalize
-    name[0, 10] if name.length > 10
+    @name = name.strip.capitalize
+    @name[0, 10] if @name.length > 10
+    @name
   end
 end
