d5099759369bd0933b9390940b40a5a2ddac4427
Ahmad Zia Yousufi <ah.ziayosfi@gmail.com>
2021-11-27 14:07:34 +0430

    add a context information about tests

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -4,28 +4,32 @@ require 'rspec'
 
 
 RSpec.describe Student do
-  before(:each) do
-    @student = Student.new("Rustam", 21, true, "Ruby")
-  end
+  
+  context “When testing the Student class” do
+    
+    before(:each) do
+      @student = Student.new("Rustam", 21, true, "Ruby")
+    end
 
-  it "should have a name" do
-    expect(@student.name).to eq("Rustam")
-  end
+    it "should have a name" do
+      expect(@student.name).to eq("Rustam")
+    end
 
-  it "should have an age" do
-    expect(@student.age).to eq(21)
-  end
+    it "should have an age" do
+      expect(@student.age).to eq(21)
+    end
 
-  it "should have a parent permission" do
-    expect(@student.parent_permission).to eq(true)
-  end
+    it "should have a parent permission" do
+      expect(@student.parent_permission).to eq(true)
+    end
 
-  it "should have a classroom" do
-    expect(@student.classroom).to eq("Ruby")
-  end
+    it "should have a classroom" do
+      expect(@student.classroom).to eq("Ruby")
+    end
 
-  it "should have a method to play hooky" do
-    expect(@student.play_hooky).to eq("¯\(ツ)/¯")
+    it "should have a method to play hooky" do
+      expect(@student.play_hooky).to eq("¯\(ツ)/¯")
+    end
   end
 end    
 
