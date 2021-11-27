58234f40781fb03c83c841a62ae5f330e239f3d2
Ahmad Zia Yousufi <ah.ziayosfi@gmail.com>
2021-11-27 14:01:03 +0430

    Remove comments

--- spec/corrector_spec.rb
+++ spec/corrector_spec.rb
@@ -1,20 +1,6 @@
 require './corrector'
 require 'rspec'
 
-# describe Corrector do
-#   before(:each) do
-#     @corrector = Corrector.new
-#   end
-
-#   it 'should capitalize first letter of each word' do
-#     expect(@corrector.correct_name('ahmad')).to eq('Ahmad')
-#   end
-
-#   it 'should capitalize first letter' do
-#     expect(@corrector.correct_name('rustamjon')).to eq('Rustamjon')
-#   end
-# end
-
 RSpec.describe Corrector do
   before(:each) do
     @corrector = Corrector.new
