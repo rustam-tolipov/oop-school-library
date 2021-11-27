d30e5fafea07281e6725113fe430a7dfd9841452
Ahmad Zia Yousufi <ah.ziayosfi@gmail.com>
2021-11-27 14:11:53 +0430

    add context info for tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,21 +2,23 @@ require './book'
 require 'rspec'
 
 RSpec.describe Book do
-  before(:each) do
-    @book = Book.new('title', 'author')
-  end
-
-  describe '#initialize' do
-    it 'should have a title' do
-      expect(@book.title).to eq('title')
+  context “When testing the Book class” do
+    before(:each) do
+      @book = Book.new('title', 'author')
     end
 
-    it 'should have an author' do
-      expect(@book.author).to eq('author')
-    end
+    describe '#initialize' do
+      it 'should have a title' do
+        expect(@book.title).to eq('title')
+      end
 
-    it 'should have no rentals' do
-      expect(@book.rentals).to eq([])
+      it 'should have an author' do
+        expect(@book.author).to eq('author')
+      end
+
+      it 'should have no rentals' do
+        expect(@book.rentals).to eq([])
+      end
     end
   end
 end
