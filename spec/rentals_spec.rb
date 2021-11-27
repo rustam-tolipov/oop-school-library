ec42b20298ff93bb737d1de932f70fb4e10945dc
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-11-27 16:15:00 +0500

    Remove Extra commits

--- spec/rentals_spec.rb
+++ spec/rentals_spec.rb
@@ -1,25 +1,3 @@
-# class Rental
-#   attr_reader :book, :person
-#   attr_accessor :date
-
-#   def initialize(date, book, person)
-#     @date = date
-#     @book = book
-#     @person = person
-#     book.rentals << self
-#     person.rentals << self
-#   end
-
-#   def to_json(*args)
-#     {
-#       JSON.create_id => self.class.name,
-#       'date' => @date,
-#       'book' => @book,
-#       'person' => @person
-#     }.to_json(*args)
-#   end
-# end
-
 require './person'
 require './student'
 require './teacher'
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,33 +1,3 @@
-# require './person'
-
-# class Student < Person
-#   attr_reader :classroom
-
-#   def initialize(name, age, parent_permission, classroom = nil)
-#     super(name, age, parent_permission)
-#     @classroom = classroom
-#   end
-
-#   def play_hooky
-#     "¯\(ツ)/¯"
-#   end
-
-#   def classroom=(classroom)
-#     @classroom = classroom
-#     @classroom.students.push(self) unless classroom.students.include?(self)
-#   end
-
-#   def to_json(*args)
-#     {
-#       JSON.create_id => self.class.name,
-#       'id' => id,
-#       'name' => name,
-#       'age' => age,
-#       'parent_permission' => @parent_permission
-#     }.to_json(*args)
-#   end
-# end
-
 require './student'
 require './person'
 require 'rspec'
